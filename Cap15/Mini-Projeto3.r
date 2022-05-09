989175551ab720c13c67a89c268dcc5cf6830f2d
Lucas Pereira Galves <lucaspereiragalves@gmail.com>
2022-05-09 20:01:16 -0300

    infos de dados

--- Cap15/Mini-Projeto3.r
+++ Cap15/Mini-Projeto3.r
@@ -25,3 +25,25 @@ library(dplyr)
 library(reshape)
 library(randomForest)
 library(e1071)
+
+
+
+
+#Carregando o dataset
+#Fonte: 
+
+
+dados_clientes <- read.csv("dataset.csv")
+
+
+#Visualiza os dados e as estruturas
+View(dados_clientes)
+
+#Verificam os tipos de dadosss
+str(dados_clientes)
+summary(dados_clientes)
+
+
+
+
+
