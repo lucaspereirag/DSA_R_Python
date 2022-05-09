8b41089b12f66dab005fa4b0a2522ae3a17074e4
Lucas Pereira Galves <lucaspereiragalves@gmail.com>
2022-05-09 19:51:47 -0300

    Initial commit

---
+++ Cap15/Mini-Projeto3.r
@@ -0,0 +1,27 @@
+#Define a pasta de trabalho
+setwd("D:/workspace/1_DSA_POWER_BI/Cap15")
+
+getwd()
+
+
+
+#Instala os pacotes para o projeto
+
+
+install.packages("Amelia") #Funções para tratar valores ausentes
+install.packages("caret")  #Construir modelos de machine learning
+install.packages("ggplot2") #Construção de gráficos
+install.packages("dplyr")   #Tratamento de dados
+install.packages("reshape")  #Modificar os formatos de dados - manipulação
+install.packages("randomForest") #Trabalhar com ML 
+install.packages("e1071")        #Trabalhar com ML
+
+
+#Carrega os pacotes
+library(Amelia)
+library(caret)
+library(ggplot2)
+library(dplyr)
+library(reshape)
+library(randomForest)
+library(e1071)
